4f17763fe27005de61c8febe6cd917490d38bdb9
Max Krivanek <thekigen@gmail.com>
2026-03-31 19:58:45 -0500

    Fix missing LICENSE headers. Fix golangci-lint issue.

--- query.go
+++ query.go
@@ -1,3 +1,19 @@
+/*
+   Copyright 2022 Max Krivanek
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 // Package t1net implements UDP query protocols for Starsiege: Tribes (Tribes 1)
 // game servers and master servers. It supports the GameSpy protocol, the native
 // Tribes ping and game info protocols, and the master server list protocol.
